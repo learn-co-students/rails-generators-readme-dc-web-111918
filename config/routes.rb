1713c598d66bd3ac9a5ba23510006a79f1b78fb7
github username <github email address>
2018-12-17 12:24:33 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+  resources :accounts
+  get 'acccounts/new'
+
+  get 'acccounts/create'
+
+  get 'acccounts/edit'
+
+  get 'acccounts/update'
+
+  get 'acccounts/destroy'
+
+  get 'acccounts/index'
+
+  get 'acccounts/show'
+
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
